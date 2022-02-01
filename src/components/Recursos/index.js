aa7e289ea37cc39ceab96b6f51eba58b11ba0c9b
Sebastian Chicoma Sandmann <sebastianchicoma97@gmail.com>
2022-02-01 15:59:11 -0500

    Recursos section

--- src/components/Recursos/index.js
+++ src/components/Recursos/index.js
@@ -1,18 +1,38 @@
 import './Recursos.css';
+import image1 from './../../images/recursos1.png';
+import image2 from './../../images/recursos2.png';
+import image3 from './../../images/recursos3.png';
 
 function Recursos() {
     return (
         <section className="Recursos">
-            <div class="container">
+            <div className="container">
                 <div className="Recursos__images">
-                    <div>
-                        <div></div>
-                        <div></div>
+                    <div className="Recursos__gallery">
+                        <div className="image-1">
+                            <picture>
+                                {/* <source media="(min-width:650px)" srcSet={image1} />
+                                <source media="(min-width:100px)" srcSet={image3} /> */}
+                                <source srcSet={image1} />
+                                <img alt="recurso 1"></img>
+                            </picture>
+                        </div>
+                        <div className="image-2">
+                            <picture>
+                                <source srcSet={image2} />
+                                <img alt="recurso 2"></img>
+                            </picture>
+                        </div>
+                        <div className="image-3">
+                            <picture>
+                                <source srcSet={image3} />
+                                <img alt="recurso 3"></img>
+                            </picture>
+                        </div>
                     </div>
-                    <div></div>
                 </div>
                 <div className="Recursos__information">
-                    <h1>Recursos gratuitos</h1>
+                    <h1 className="Recursos__information-title">Recursos gratuitos</h1>
                 </div>
             </div>
         </section>
@@ -20,3 +40,4 @@ function Recursos() {
 }
 
 export { Recursos };
+
